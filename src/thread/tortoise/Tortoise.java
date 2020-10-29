d77dcf84d59950accbd1379db2d4f1e79f321893
Harsh Gupta <harsh.gupta_cs19@gla.ac.in>
2020-10-29 16:45:59 -0400

    Add a switch block

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -3,4 +3,33 @@ package thread.tortoise;
 import javax.swing.*;
 
 public class Tortoise extends Thread {
+
+    private static final int MILESTONES = 5;
+
+    public Tortoise() {
+        super("tortoise");
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
+        }
+    }
 }
